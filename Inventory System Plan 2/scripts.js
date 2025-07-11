8237220d4c4c6d015d46d8e79a362acc15ada8ef
Muhammad Iqbal Taufik <58700500+IqbaalTaufik@users.noreply.github.com>
2025-07-11 10:01:39 +0700

    Progress 1
    membuat navbar dan style navbar
    membuat halaman login(edited)

---
+++ Inventory System Plan 2/scripts.js
@@ -0,0 +1,26 @@
+
+// <-------------------------------------------------------------------------->
+// Login function(doing back to main menu)
+// // Get the modal
+    // var modal = document.getElementById('id001');
+
+    // // When the user clicks anywhere outside of the modal, close it
+    // window.onclick = function(event) {
+    // if (event.target == modal) {
+    //     modal.style.display = "none";
+    // }
+    // }
+function redirect() {
+    window.location.href='index.html';
+}
+// function lokhin() {
+//     let text;
+//     if (document.getElementById='usrname'="admin"){
+//         if (document.getElementById='psword'="123456"){
+//             Text = "berhasil login";
+//         }
+//         else{
+//             Text = "salah password cok";
+//         }
+//     }
+// }
